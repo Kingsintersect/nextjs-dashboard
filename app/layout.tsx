033aa6f3ce0c5f8d035c70706c6ae87f2e9285db
kingsintersect <kingsintersect@gmail.com>
2024-04-30 16:10:26 +0100

    added metadata to all my page for SEO features

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,22 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, Built with App Router',
-  metadataBase: new URL('https://next-learn-dashboard.versel.sh')
-}
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
